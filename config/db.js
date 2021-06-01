df9a74bd815008cba352588faadce2fb7bb5ae62
BorislavBranimirov <boriintorr@gmail.com>
2021-06-01 20:52:44 +0100

    fix ssl connection issue

--- config/db.js
+++ config/db.js
@@ -3,13 +3,19 @@ const { Pool } = require('pg');
 // connect with either URI or connection parameters
 // URI takes precedence if both methods are provided
 const config = (process.env.PG_URI) ? ({
-    connectionString: process.env.PG_URI
+    connectionString: process.env.PG_URI,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }) : ({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DB,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const pool = new Pool(config);
