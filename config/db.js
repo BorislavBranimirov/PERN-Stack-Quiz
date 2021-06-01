439cf7648dab87962e19c53bfc250003fe0c63c8
BorislavBranimirov <boriintorr@gmail.com>
2021-06-01 19:53:13 +0100

    formatting

--- config/db.js
+++ config/db.js
@@ -13,8 +13,8 @@ const config = (process.env.PG_URI) ? ({
 });
 
 const pool = new Pool(config);
-pool.connect((err)=>{
-    if(err){
+pool.connect((err) => {
+    if (err) {
         return console.error('Error acquiring client', err.stack);
     }
 })
